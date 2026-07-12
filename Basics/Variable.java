8a78075ac5ca4a3a2e7bc16b40fddb23b89b00f7
usep-arch <s93753564@gmail.com>
2026-07-12 07:38:57 +0600

    feat: Variable in Java Language

---
+++ Basics/Variable.java
@@ -0,0 +1,29 @@
+// Variable.java
+
+/*
+ - Rule : datatype variableName = value;
+ */
+public class Variable {
+    public static void main(String[] args) {
+        // Integer 
+        int age = 17; 
+        System.out.println("My age is " + age);
+        
+        // Double - Decimal number
+        double price = 5.99;
+        System.out.println("Pizza price is $" + price);
+        
+        // Character - Inside of single quote
+        char grade = 'A';
+        System.out.println("My grade is " + grade);
+        
+        // String - Text type (Use double quote)
+        String name = "Siyam";
+        System.out.println("My name is " + name);
+        
+        // Boolean - true / false 
+        boolean isStudent = true;
+        System.out.println("Is siyam a student? " + isStudent);
+        
+    }
+}
